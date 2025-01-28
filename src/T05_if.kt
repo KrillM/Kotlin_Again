a922fb3c3f81180205c89fa3b1bb37affadc0ab0
krille <mks160615@gmail.com>
2025-01-28 16:32:43 +0900

    if

---
+++ src/T05_if.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    var score = 80
+    score = 55
+
+    if (score >= 70) {
+        println("Pass")
+    }
+    else {
+        println("Fail")
+    }
+
+    var score2 = 80
+
+    if (score2 >= 90) {
+        println("A")
+    }
+    else if (score2 >= 70 && score2 < 90) {
+        println("B")
+    }
+    else {
+        println("F")
+    }
+}
+
