48824ed25f80be34a7c572195160566a3868d010
krille <mks160615@gmail.com>
2025-01-28 15:42:48 +0900

    while and do whlie

---
+++ src/T04_While.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    // While
+    var i = 1
+    while(i<5){
+        println(i)
+        i++
+    }
+
+    // do_While
+    var j = 5;
+    do {
+        println(j)
+        j++
+    } while (j<5)
+}
