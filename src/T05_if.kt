76168f2685d927c3c378c0ef594666064233c705
krille <mks160615@gmail.com>
2025-01-28 17:48:42 +0900

    recode if

--- src/T05_if.kt
+++ src/T05_if.kt
@@ -1,21 +1,48 @@
 fun main() {
 
+    // 예시 1
     var score = 80
-    score = 55
 
-    if (score >= 70) {
+    if (score >= 70) { // if문의 첫 번째 조건이다. 해당 조건이 맞으면 Pass가 출력된다.
         println("Pass")
+    }
+    else {
+        println("Fail") // 모든 조건을 만족하지 않는다면 else문이 출력된다. Fail이 출력된다.
+    }
+
+    // 예시 2
+    var score2 = 95
+
+    if (score2>= 90) {
+        println("A")
+    }
+    if (score2>= 70 && score2 < 90) {
+        println("B")
     }
     else {
         println("Fail")
     }
 
-    var score2 = 80
+    // 예시 3
+    var score3 = 95
 
-    if (score2 >= 90) {
+    if (score3 >= 90) {
         println("A")
     }
-    else if (score2 >= 70 && score2 < 90) {
+    else if (score3 >= 70 && score3 < 90) {
+        println("B")
+    }
+    else {
+        println("F")
+    }
+
+    // 예시 4
+    score3 = 77
+
+    if (score3 >= 90) {
+        println("A")
+    }
+    else if (score3 >= 70 && score3 < 90) {
         println("B")
     }
     else {
