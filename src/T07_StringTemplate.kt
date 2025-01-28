63204f7d032856187b033c1fd215e58c0345e6d3
krille <mks160615@gmail.com>
2025-01-29 02:16:37 +0900

    string template

---
+++ src/T07_StringTemplate.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var name = "Krille"
+    println("My name is "+name)
+    println("My name is $name")
+
+    var num1 = 15
+    var num2 = 2
+    println("Add : "+(num1 + num2))
+    println("Add : ${num1 + num2}")
+}
